6898c454dfc07abc4626bf798f16d2982934c1a0
spock254 <qwertyspock254@gmail.com>
2019-03-17 17:01:36 +0300

    fix trans pos door bug

--- proto1/Assets/script/world/door/DoorManager.cs
+++ proto1/Assets/script/world/door/DoorManager.cs
@@ -23,50 +23,65 @@ public class DoorManager : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         insaideCollider2D.enabled = false;
     }
+    private void Update()
+    {
+        // TODO 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float distanceOutside = Vector2.Distance(player.transform.position, outsideSpownPoint.position);
+            float distanceInside = Vector2.Distance(player.transform.position, insideSpownPoint.position);
+            if (distanceOutside > distanceInside)
+                isOutside = false;
+            else
+                isOutside = true;
+        }
 
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
+       
+
         if (Input.GetKeyDown(KeyCode.E) && isOutside)
         {
             player.transform.position = insideSpownPoint.position;
-            switchFade();
-            insaideCollider2D.enabled = false;
-            outsaideCollider2D.enabled = true;
-            isOutside = false;
+            InsadeFade();
+            insaideCollider2D.enabled = true;
+            outsaideCollider2D.enabled = false;
+           // isOutside = false;
             animator.SetBool("open", true);
             Debug.Log("Entered");
         }
         else if (Input.GetKeyDown(KeyCode.E) && !isOutside)
         {
             player.transform.position = outsideSpownPoint.position;
-            switchFade();
-            insaideCollider2D.enabled = true;
-            outsaideCollider2D.enabled = false;
-            isOutside = true;
+            OutsideFade();
+            insaideCollider2D.enabled = false;
+            outsaideCollider2D.enabled = true;
+            //isOutside = true;
             animator.SetBool("open", true);
             Debug.Log("Exit");
         }
+        
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         animator.SetBool("open", false);
+        Debug.Log("X");
+        //isOutside = !isOutside;
     }
-    private void switchFade()
+    private void InsadeFade()
     {
-        if (isOutside)
-        {
-            //TODO lerp fade
-            foreach (GameObject fade in outsideFades)
-                fade.SetActive(isOutside);
-            foreach (GameObject fade in insidedeFades)
-                fade.SetActive(!isOutside);
-        }
-        else
-        {
-            foreach (GameObject fade in outsideFades)
-                fade.SetActive(!isOutside);
-            foreach (GameObject fade in insidedeFades)
-                fade.SetActive(isOutside);
-        }
+        foreach (GameObject fade in outsideFades)
+            fade.SetActive(true);
+        foreach (GameObject fade in insidedeFades)
+            fade.SetActive(false);
     }
+    private void OutsideFade()
+    {
+        foreach (GameObject fade in outsideFades)
+            fade.SetActive(false);
+        foreach (GameObject fade in insidedeFades)
+            fade.SetActive(true);
+    }
+    
 }
