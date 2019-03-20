7af2c9fd6724617d827bdd726ec9c29ec93d0a2c
spock254 <qwertyspock254@gmail.com>
2019-03-20 14:19:25 +0300

    added head prefs

---
+++ proto1/Assets/script/player/player_gen/PlayerGeneration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGeneration : MonoBehaviour
+{
+    public List<GameObject> player_heads;
+    public GameObject head;
+
+    private GameObject head_container;
+
+    int i = 0;
+    void Start()
+    {
+        head_container = GameObject.FindGameObjectWithTag("head_container");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Destroy(head);
+            head = Instantiate(player_heads[i]);
+            i++;
+            head.transform.SetParent(head_container.transform);
+        }
+    }
+}
