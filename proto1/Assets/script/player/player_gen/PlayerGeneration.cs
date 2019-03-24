cf8b3ac39847e39c3fd33e742982f24818b4177f
spock254 <qwertyspock254@gmail.com>
2019-03-24 17:12:05 +0300

    random player color

--- proto1/Assets/script/player/player_gen/PlayerGeneration.cs
+++ proto1/Assets/script/player/player_gen/PlayerGeneration.cs
@@ -54,13 +54,15 @@ public class PlayerGeneration : MonoBehaviour
     private void Start()
     {
         int index = Random.Range(0, player_heads.Count);
+        int color_skin_index = Random.Range(0, skin_colors.Count);
 
-        RandomizePlayerPart(player_heads_bg, head_bg_container, head_bg, index); // head bg
-        RandomizePlayerPart(player_heads, head_container, head, index); // head
-        RandomizePlayerPart(player_body, body_conainer, body); // body
-        RandomizePlayerPart(player_lags, lags_container, lags); // lags
-        RandomizePlayerPart(player_hands_left, hands_container, hands_left); // hand left
-        RandomizePlayerPart(player_hands_right, hands_container, hands_right); // hand right
+        RandomizePlayerPart(player_heads_bg, head_bg_container, head_bg, index, color_skin_index); // head bg
+        RandomizePlayerPart(player_heads, head_container, head, index, hair_colors); // head
+        RandomizePlayerPart(player_body, body_conainer, body, body_colors); // body
+        RandomizePlayerPart(player_lags, lags_container, lags, lags_colors); // lags
+        RandomizePlayerPart(player_hands_left, hands_container, hands_left, color_skin_index); // hand left
+        RandomizePlayerPart(player_hands_right, hands_container, hands_right, color_skin_index); // hand right
+
     }
     void Update()
     {
@@ -69,21 +71,41 @@ public class PlayerGeneration : MonoBehaviour
         AnimController(moveX, moveY);
         AnimSet();
     }
-    void RandomizePlayerPart(List<GameObject> contents,GameObject container,GameObject current_part)
+    void RandomizePlayerPart(List<GameObject> contents,GameObject container,GameObject current_part,List<Color> colors)
     {
         Destroy(current_part);
         current_index = Random.Range(0, contents.Count);
         current_part = Instantiate(contents[current_index]);
         current_part.transform.SetParent(container.transform);
         animators.Add(current_part.GetComponent<Animator>());
+        ColorRandomize(current_part, colors);
     }
-    void RandomizePlayerPart(List<GameObject> contents, GameObject container, GameObject current_part,int index)
+    void RandomizePlayerPart(List<GameObject> contents, GameObject container, GameObject current_part, int color_index)
+    {
+        Destroy(current_part);
+        current_index = Random.Range(0, contents.Count);
+        current_part = Instantiate(contents[current_index]);
+        current_part.transform.SetParent(container.transform);
+        animators.Add(current_part.GetComponent<Animator>());
+        ColorRandomize(current_part, skin_colors[color_index]);
+    }
+    void RandomizePlayerPart(List<GameObject> contents, GameObject container, GameObject current_part,int index, List<Color> colors)
     {
         Destroy(current_part);
         current_index = index;
         current_part = Instantiate(contents[current_index]);
         current_part.transform.SetParent(container.transform);
         animators.Add(current_part.GetComponent<Animator>());
+        ColorRandomize(current_part, colors);
+    }
+    void RandomizePlayerPart(List<GameObject> contents, GameObject container, GameObject current_part, int index, int color_index)
+    {
+        Destroy(current_part);
+        current_index = index;
+        current_part = Instantiate(contents[current_index]);
+        current_part.transform.SetParent(container.transform);
+        animators.Add(current_part.GetComponent<Animator>());
+        ColorRandomize(current_part, skin_colors[color_index]);
     }
 
     private void AnimController(float x, float y)
@@ -112,5 +134,20 @@ public class PlayerGeneration : MonoBehaviour
             animator.SetBool("left", false);
             animator.SetBool("right", false);
         }
+    }
+    private void ColorRandomize(GameObject part, List<Color> colors)
+    {
+        part.GetComponent<SpriteRenderer>().color = colors[Random.Range(0, colors.Count)];
+    }
+    private void ColorRandomize(GameObject part, Color color)
+    {
+        part.GetComponent<SpriteRenderer>().color = color;
+    }
+    private void ColorRandomize(GameObject part,GameObject part2,GameObject part3, List<Color> colors)
+    {
+        int newColor_index = Random.Range(0, colors.Count);
+        part.GetComponent<SpriteRenderer>().color = colors[newColor_index];
+        part2.GetComponent<SpriteRenderer>().color = colors[newColor_index];
+        part3.GetComponent<SpriteRenderer>().color = colors[newColor_index];
     }
 }
