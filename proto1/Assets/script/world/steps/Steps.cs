f15d998c736b0e80fcfe5f98104a37df28f8762c
spock254 <qwertyspock254@gmail.com>
2019-03-25 17:58:36 +0300

    ginam base steps

---
+++ proto1/Assets/script/world/steps/Steps.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steps : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Color alpha;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        alpha = spriteRenderer.color;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        alpha.a += 0.01f;
+        spriteRenderer.color = alpha;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        alpha = spriteRenderer.color;
+    }
+}
